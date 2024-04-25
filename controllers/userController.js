43af37bb18e13b4469f90e36c442ece8449ea809
Jonah-john-thangadurai <55949424+Jonah411@users.noreply.github.com>
2024-04-25 10:01:59 +0530

    update24-04-2024

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,10 @@
-const getAllUser = (req, res) => {
-  res.status(200).json({ message: "Get all user" });
-};
+const asyncHandler = require("express-async-handler");
 
-const createUser = (req, res) => {
+const getAllUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Get all user" });
+});
+
+const createUser = asyncHandler(async (req, res) => {
   console.log("create user:", req.body);
   const { name, age, phone, email } = req.body;
   console.log(name, age, phone, email);
@@ -12,18 +14,18 @@ const createUser = (req, res) => {
     throw new Error("All fiels are mandatory");
   }
   res.status(201).json({ message: "Create user" });
-};
+});
 
-const updateUser = (req, res) => {
+const updateUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update user for ${req.params.id}` });
-};
+});
 
-const deleteUser = (req, res) => {
+const deleteUser = asyncHandler(async (req, res) => {
   res.status(201).json({ message: `Delete user for ${req.params.id}` });
-};
+});
 
-const getUser = (req, res) => {
+const getUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Get user for ${req.params.id}` });
-};
+});
 
 module.exports = { getAllUser, createUser, updateUser, deleteUser, getUser };
