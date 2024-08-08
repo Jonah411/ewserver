04200e1924c0c37df850923e25c7fee99885ea33
Jonah-john-thangadurai <55949424+Jonah411@users.noreply.github.com>
2024-08-09 04:02:36 +0530

    update Code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,11 +6,14 @@ const {
   updateUser,
   deleteUser,
   getUser,
+  getUserOrg,
 } = require("../controllers/userController");
 
 //router.route("/").get(getAllUser).post(createUser);
 router.route("/createuser").post(createUser);
 
 router.route("/:id").get(getUser).put(updateUser).delete(deleteUser);
+
+router.route("/getorg/:id").get(getUserOrg);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 
 const cors = require("cors");
 const corsOptions = require("./config/CorsOptions");
+const authMiddleware = require("./middleware/authMiddleware");
 app.use(cors(corsOptions));
 
 const port = process.env.PORT || 5000;
@@ -16,11 +17,15 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use("/app/image", express.static(path.join(__dirname, "public/logo")));
 
-app.use("/app/user", require("./routes/userRoutes"));
-//app.use("/app/auth", require("./routes/authRoutes"));
+app.use("/app/user/", require("./routes/userRoutes"));
+app.use("/app/auth", require("./routes/authRoutes"));
 app.use("/app/roll", require("./routes/rollRoutes"));
 app.use("/app/org/", require("./routes/orgRoutes"));
 app.use(errorHandler);
+
+app.get("/api/protected", authMiddleware, (req, res) => {
+  res.json({ message: "This is a protected route", user: req.user });
+});
 
 app.listen(port, () => {
   console.log(`server running on ${port}`);
